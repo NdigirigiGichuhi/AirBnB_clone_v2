ae47e856cdaa577074ae65d0b8639ef4db5c1a55
Erick Awino <erick.otieno89@gmail.com>
2024-02-03 13:55:21 +0300

    Task 3 Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,49 +3,51 @@
 
 from fabric.api import *
 from datetime import datetime
-from os import path, remove
+from os import path
 
 env.hosts = ['34.234.203.177', '52.72.75.78']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
-def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    # obtain the current date and time
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # Construct path where archive will be saved
-    archive_path = "versions/web_static_{}.tgz".format(now)
-
-    # use fabric function to create directory if it doesn't exist
-    local("mkdir -p versions")
-
-    # Use tar command to create a compresses archive
-    archived = local("tar -cvzf {} web_static".format(archive_path))
-
-    # Check archive Creation Status
-    if archived.return_code != 0:
-        return None
-    else:
-        return archive_path
-
-
 def do_deploy(archive_path):
-    '''use os module to check for valid file path'''
-    if os.path.exists(archive_path):
-        archive = archive_path.split('/')[1]
-        a_path = "/tmp/{}".format(archive)
-        folder = archive.split('.')[0]
-        f_path = "/data/web_static/releases/{}/".format(folder)
+    """
+    Deploy web files to the server
+    """
+    try:
+        if not path.exists(archive_path):
+            return False
 
-        put(archive_path, a_path)
-        run("mkdir -p {}".format(f_path))
-        run("tar -xzf {} -C {}".format(a_path, f_path))
-        run("rm {}".format(a_path))
-        run("mv -f {}web_static/* {}".format(f_path, f_path))
-        run("rm -rf {}web_static".format(f_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(f_path))
-        return True
-    return False
+        # Upload archive
+        put(archive_path, '/tmp/')
+
+        # Create target directory
+        timestamp = archive_path[-18:-4]
+        release_path = ('/data/web_static/releases/web_static_{}/'
+                        .format(timestamp))
+        run('sudo mkdir -p {}'.format(release_path))
+
+        # Uncompress archive and delete .tgz
+        run('sudo tar -xzf /tmp/web_static_{0}.tgz -C {1}'
+            .format(timestamp, release_path))
+        # Remove archive
+        run('sudo rm /tmp/web_static_{0}.tgz'.format(timestamp))
+
+        # Move contents into host web_static
+        run('sudo mv {0}/web_static/* {0}/'.format(release_path))
+
+        # Remove extraneous web_static directory
+        run('sudo rm -rf {0}/web_static'.format(release_path))
+
+        # Delete pre-existing symbolic link
+        run('sudo rm -rf /data/web_static/current')
+
+        # Re-establish symbolic link
+        current_path = '/data/web_static/current'
+        run('sudo ln -s {0} {1}'.format(release_path, current_path))
+
+    except Exception as e:
+        return False
+
+    # Return True on success
+    return True
