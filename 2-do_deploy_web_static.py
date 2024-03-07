16a945d1b96ef55a22aa88c927a5efc42a124d51
Erick Awino <erick.otieno89@gmail.com>
2024-03-07 22:02:22 +0300

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """A Fabric Script that distributes archives on web servers"""
 
-from fabric.api import *
-from datetime import datetime
+from fabric.api import env, run, put
 from os import path
 
 env.hosts = ['18.215.160.180', '3.95.32.201']
@@ -11,41 +10,38 @@ env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """
-    Deploying the  web files to the server
+    """ Distributes an archive to each web server
+
+    Arguments:
+        archive_path (str): The path of the archive to distribute
+    Returns:
+        If file doesn't exist at path - False
+        Otherwise - True
     """
     if not path.exists(archive_path):
         return False
+
     try:
-        # Upload archive
-        put(archive_path, '/tmp/')
+        put(archive_path, '/tmp')
 
-        # Create target directory
-        timestamp = archive_path[-18:-4]
-        release_path = ('/data/web_static/releases/web_static_{}/'
-                        .format(timestamp))
-        run('mkdir -p {}'.format(release_path))
+        # Uncompress archive to the folder
+        archive_filename = path.basename(archive_path)
+        release_folder = '/data/web_static/releases/{}'.format(
+            archive_filename.split('.')[0])
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
 
-        # Uncompress archive and delete .tgz
-        run('tar -xzf /tmp/web_static_{0}.tgz -C {1}'
-            .format(timestamp, release_path))
-        # Remove archive
-        run('rm /tmp/web_static_{0}.tgz'.format(timestamp))
+        # Delete the archive from web server
+        run('rm /tmp/{}'.format(archive_filename))
 
-        # Move contents into host web_static
-        run('mv {0}/web_static/* {0}/'.format(release_path))
-
-        # Remove extraneous web_static directory
-        run('rm -rf {0}/web_static'.format(release_path))
-
-        # Delete pre-existing symbolic link
+        # Delete the symbolic link /data/web_static/current
         run('rm -rf /data/web_static/current')
 
-        # Re-establish symbolic link
-        current_path = '/data/web_static/current'
-        run('ln -s {0} {1}'.format(release_path, current_path))
+        # Create a new the symbolic link /data/web_static/current
+        run('ln -s {} /data/web_static/current'.format(release_folder))
 
-        print("New version deployed!")
+        print('New version deployed!')
         return True
     except Exception as e:
+        print('Error:', e)
         return False
