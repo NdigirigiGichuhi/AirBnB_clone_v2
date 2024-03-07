99f51a3b4c47ad873ae70aa453ce88e821957cf6
Erick Awino <erick.otieno89@gmail.com>
2024-03-07 08:19:51 +0300

    compressing the files using fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,18 +2,19 @@
 from fabric.api import local
 from time import strftime
 from datetime import date
-
+import os
 
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
 
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("sudo mkdir -p versions")
-        local("sudo tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+    local("sudo mkdir -p versions")
 
-        return "versions/web_static_{}.tgz".format(filename)
+    file_name = strftime("%Y%m%d%H%M%S")
 
-    except Exception as e:
-        return None
+    local("sudo tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(file_name))
+    file_path = f"versions/web_static_{file_name}.tgz"
+    file_size = os.path.getsize(file_path)
+    print(f"web_static packed: {file_path}.tgz -> {file_size}Bytes")
+
+#  return "versions/web_static_{}.tgz".format(filename)
