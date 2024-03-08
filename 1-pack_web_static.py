58346d714e3f12ebd364eed3c51b713034e6c314
Erick Awino <erick.otieno89@gmail.com>
2024-03-08 07:02:37 +0300

    Comments added

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,16 +11,24 @@ def do_pack():
     file_name = strftime("%Y%m%d%H%M%S")
 
     try:
+        # Create a versions directory if it doesn't exist
         local("sudo mkdir -p versions")
 
+        # Formatting the time stamp
         file_name = strftime("%Y%m%d%H%M%S")
 
+        # Compressing the web_static folder into .tgz archive
         local(f"sudo tar -czvf versions/web_static_{file_name}.tgz \
                 web_static/")
+
+        # Defining the archive name
         file_path = f"versions/web_static_{file_name}.tgz"
+
+        # Getting the file size
         file_size = os.path.getsize(file_path)
         print(f"web_static packed: {file_path}.tgz -> {file_size}Bytes")
 
+        # Returning the archive path
         return "versions/web_static_{}.tgz".format(file_name)
     except Exception as e:
         return none
