97584f078d0c2cfd0987cad7adc3101e0f41aef0
Erick Awino <erick.otieno89@gmail.com>
2024-03-24 21:18:56 +0300

    changes

--- models/city.py
+++ models/city.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from sqlachemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from models.place import Place
+from sqlalchemy.orm import relationship
+from models.place import Place
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = Column(String(60), nullable=False, ForeignKey=("states_id"))
-    name = Column(String(128), nullable=False)
     __tablename__ = "cities"
 
-    places = relationship("Place", cascade="all, delete_orphan"), backref = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+
+    places = relationship("Place", backref="cities")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes city"""
+        supper().__init__(*args, **kwargs)
