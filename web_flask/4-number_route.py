e3b2c7c8c23a5e3c8dcc7391298926e45fbfc7d4
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-03-25 21:01:53 +0300

    number 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/python/")
+@app.route("/python/<text>", strict_slashes=False)
+def my_text(text='is cool'):
+    """Return C followed by value of text"""
+
+    if "_" in text:
+        text = text.split("_")
+        text = ",".join(text)
+        text = text.replace(",", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """Returns a number"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
