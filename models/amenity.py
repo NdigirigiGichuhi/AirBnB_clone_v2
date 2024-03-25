9e2ced132a3661f55054d49576a482f5e96455b2
Erick Awino <erick.otieno89@gmail.com>
2024-03-25 10:28:25 +0300

    Small changes

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class Amenity(BaseModel, Base):
     """class amenity"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
