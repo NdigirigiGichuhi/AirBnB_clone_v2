dcb5f02d224c1ed57b46438b3cc46997f608f25d
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-03-25 20:34:41 +0300

    question 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def my_text(text='is cool'):
+    """Return C followed by value of text"""
+
+    if "_" in text:
+        text = text.split("_")
+        text = ",".join(text)
+        text = text.replace(",", " ")
+    return f"C {text}"
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
